d43feeaa7c44ec156aa32d649b4ba964bdc8d4a1
Andrey Murashev <andreymurashev@gmail.com>
2024-08-30 13:26:12 +0400

    Components: Add components page

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,5 +1,29 @@
-'use client'
+import Link from 'next/link'
+
+import { componentPageRoute } from '@/constants/routes'
+
+import { components } from './data'
 
 export default function Page() {
-  return <div className="w-full h-full px-4 py-4 space-y-4">Select any</div>
+  return (
+    <div className="w-full h-full px-4 py-4 space-y-4">
+      <h1 className="font-bold text-lg">Select any</h1>
+      <ul className="list-disc pl-4">
+        {['conversation-card', 'chat-message', 'job-card'].map((index) => {
+          const url = componentPageRoute.getUrl({
+            params: {
+              slug: index,
+            },
+          })
+          return (
+            <li className="">
+              <Link className="h-full w-full block" href={url}>
+                {components[index as keyof typeof components].name}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
