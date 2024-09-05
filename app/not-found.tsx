22d046e55463ffc46b149d5a826e2c7b1644a6fe
Andrey Murashev <andreymurashev@gmail.com>
2024-09-05 12:07:19 +0400

    Add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div className="grid h-[100vh] w-full items-center justify-center text-center">
+      <div>
+        <h2 className="text-3xl font-bold">Not Found</h2>
+        <p className="text-muted-foreground text-xl">
+          Could not find requested resource
+        </p>
+        <Link href="/" className="text-link mt-2 block">
+          Return Home
+        </Link>
+      </div>
+    </div>
+  )
+}
