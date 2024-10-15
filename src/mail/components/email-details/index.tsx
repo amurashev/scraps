ca51710b2c1df3cd217674c1e60cc7d02982a1a7
Andrey Murashev <andreymurashev@gmail.com>
2024-10-15 12:59:11 +0400

    Emails: finish

--- src/mail/components/email-details/index.tsx
+++ src/mail/components/email-details/index.tsx
@@ -1,13 +1,17 @@
 import { format } from 'date-fns'
-import { Email } from '../../types'
+
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
+
+import { Email } from '../../types'
 
 export default function EmailDetails({ email }: { email: Email }) {
   const formattedDate = format(new Date(email.datetime), 'dd MMM HH:mm')
   return (
     <div>
-      <div className="border-b-0 border-border py-3 px-4 gap-2">
+      <div className="border-b border-border py-3 px-4 gap-2">
         <div className="text-lg font-semibold">{email.subject}</div>
+      </div>
+      <div className="p-4 gap-3 flex flex-col">
         <div className="flex flex-row items-center gap-2">
           <Avatar size={10} className="flex-shrink-0">
             <AvatarImage src={email.avatarUrl} />
@@ -17,8 +21,6 @@ export default function EmailDetails({ email }: { email: Email }) {
             {formattedDate}
           </div>
         </div>
-      </div>
-      <div className="p-4">
         <div className="text-sm text-gray-500">{email.text}</div>
       </div>
     </div>
